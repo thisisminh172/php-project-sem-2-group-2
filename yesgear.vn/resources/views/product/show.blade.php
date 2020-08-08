0cef9d3d26e33e1d27175fe2f76539d16a306c38
Babycry3508 <dcduong.qlcn@gmail.com>
2020-08-08 11:26:50 +0700

    show

--- yesgear.vn/resources/views/product/show.blade.php
+++ yesgear.vn/resources/views/product/show.blade.php
@@ -2,4 +2,38 @@
 
 @section('content')
     product list 123
+    <div class="container">
+        <div class="row">
+            <div class="col-6 col-sm-4 col-md-3 p-1">
+                <img class="" id="det_img_18418"
+                    src="http://chiemtaimobile.vn/images/thumbnails/235/235/detailed/23/may-chieu-mini-wanbo-q6a-1.jpg"
+                    alt="Máy chiếu mini Wanbo Q6A" title="Máy chiếu mini Wanbo Q6A">
+                <a href="">Máy chiếu mini Wanbo Q6A</a>
+                <a href="">Gia: </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 p-1">
+                <img class="" id="det_img_18418"
+                    src="http://chiemtaimobile.vn/images/thumbnails/235/235/detailed/23/may-chieu-mini-wanbo-q6a-1.jpg"
+                    alt="Máy chiếu mini Wanbo Q6A" title="Máy chiếu mini Wanbo Q6A">
+                <a href="">Máy chiếu mini Wanbo Q6A</a>
+                <a href="">Gia: </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 p-1">
+                <img class="" id="det_img_18418"
+                    src="http://chiemtaimobile.vn/images/thumbnails/235/235/detailed/23/may-chieu-mini-wanbo-q6a-1.jpg"
+                    alt="Máy chiếu mini Wanbo Q6A" title="Máy chiếu mini Wanbo Q6A">
+                <a href="">Máy chiếu mini Wanbo Q6A</a>
+                <a href="">Gia: </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 p-1">
+                <img class="" id="det_img_18418"
+                    src="http://chiemtaimobile.vn/images/thumbnails/235/235/detailed/23/may-chieu-mini-wanbo-q6a-1.jpg"
+                    alt="Máy chiếu mini Wanbo Q6A" title="Máy chiếu mini Wanbo Q6A">
+                <a href="">Máy chiếu mini Wanbo Q6A</a>
+                <a href="">Gia: </a>
+            </div>
+        </div>
+    </div>
+
+
 @endsection
