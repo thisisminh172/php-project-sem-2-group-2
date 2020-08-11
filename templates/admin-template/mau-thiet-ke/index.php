ad5f07e3f8504124de4123a71aa961fe7d60699e
thisisminh172 <thisisminh172@gmail.com>
2020-08-11 09:47:30 +0700

    nothing

---
+++ templates/admin-template/mau-thiet-ke/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mau thiet ke</title>
+    <!-- bootstrap start-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- bootstrap end-->
+
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <!-- style -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- header start -->
+    <div id="wp-header" class="bg-dark">
+        <a href="" class="display-4 font-weight-bold text-decoration-none"><span
+                            class="text-light">HELLO</span><span class="text-danger">ADMIN</span></a>
+    </div>
+    <!-- header end -->
+
+    <!-- sidebar start -->
+    <div id="wp-sidebar" class="bg-light">
+        <div id="sidebar-menu">
+            <ul class="list-unstyled">
+                <li>
+                <i class="far fa-folder"></i><a href="">Dashboard</a>
+
+                </li>
+                <li>
+                <i class="far fa-folder"></i><a href="">Sản phẩm</a>
+                    <ul class="list-unstyled">
+                        <li><a href="">Thêm sản phẩm</a></li>
+                        <li><a href="">Danh sách</a></li>
+                        <li><a href="">Gửi thông báo</a></li>
+                    </ul>
+
+                </li>
+                <li>
+                <i class="far fa-folder"></i><a href="">Khách hàng</a>
+                    <ul class="list-unstyled">
+                        <li><a href="">Danh sách</a></li>
+                        <li><a href="">Cập nhật</a></li>
+                        <li><a href="">Gửi thông báo</a></li>
+                    </ul>
+                </li>
+                <li>
+                <i class="far fa-folder"></i><a href="">Trang</a>
+                    <ul class="list-unstyled">
+                        <li><a href="">Danh sách</a></li>
+                        <li><a href="">Cập nhật</a></li>
+                    </ul>
+                </li>
+                <li>
+                <i class="far fa-folder"></i><a href="">Tin tức</a>
+                    <ul class="list-unstyled">
+                        <li><a href="">Danh sách</a></li>
+                        <li><a href="">Cập nhật</a></li>
+                    </ul>
+                </li>
+                <li>
+                <i class="far fa-folder"></i><a href="">Bán hàng</a>
+                    <ul class="list-unstyled">
+                        <li><a href="">Đơn hàng</a></li>
+                        <li><a href="">Cập nhật</a></li>
+                    </ul>
+                </li>
+                <li>
+                <i class="far fa-folder"></i><a href="">Users</a>
+                    <ul class="list-unstyled">
+                        <li><a href="">Danh sách</a></li>
+                        <li><a href="">Cập nhật</a></li>
+                        <li><a href="">Gửi thông báo</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- sidebar end -->
+
+
+
+
+
+
+
+
+    <!-- bootstrap start-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+    </script>
+    <!-- bootstrap end-->
+</body>
+
+</html>
