b9b20312f1e4e89954ad6cb31a87b572c31c8106
thisisminh172 <thisisminh172@gmail.com>
2020-08-13 10:15:53 +0700

    change migration order

--- yesgear.vn/app/Order.php
+++ yesgear.vn/app/Order.php
@@ -7,9 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     //
-    public function customer(){
-        return $this->belongsTo('App\Customer');
-    }
 
     public function order_detail(){
         return $this->hasMany('App\OrderDetail');
--- yesgear.vn/database/migrations/2020_09_12_113558_create_orders_table.php
+++ yesgear.vn/database/migrations/2020_09_12_113558_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCustomersTable extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCustomersTable extends Migration
      */
     public function up()
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('name',100);
             $table->string('email',150);
@@ -30,6 +30,6 @@ class CreateCustomersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('orders');
     }
 }
