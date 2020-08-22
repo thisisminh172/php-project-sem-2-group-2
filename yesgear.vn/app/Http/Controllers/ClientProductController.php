b2c9de30481d308c4e91fcd8fbc104dbc05ce85b
thisisminh172 <thisisminh172@gmail.com>
2020-08-22 21:33:47 +0700

    fixed bug search box

--- yesgear.vn/app/Http/Controllers/ClientProductController.php
+++ yesgear.vn/app/Http/Controllers/ClientProductController.php
@@ -2,45 +2,46 @@
 
 namespace App\Http\Controllers;
 
-use App\Product;
-use Illuminate\Http\Request;
 use App\CustomClass\ShowProductWithCondition;
+use App\Product;
 use DB;
+use Illuminate\Http\Request;
 
 class ClientProductController extends Controller
 {
     //goi truoc khi
 
-
     //viet ham tu day
-    public function index(){
-        $p = new ShowProductWithCondition;
+    public function index()
+    {
+        $p = new ShowProductWithCondition();
         $products = Product::all();
         $headphones = $p->show_headphone();
         $keyboards = $p->show_keyboard();
         $mouses = $p->show_mouse();
-        return view('client.home.home')->with(['products'=>$products,'headphones'=>$headphones,'keyboards'=>$keyboards,'mouses'=>$mouses]);
+        return view('client.home.home')->with(['products' => $products, 'headphones' => $headphones, 'keyboards' => $keyboards, 'mouses' => $mouses]);
     }
-    public function show(){
+
+    public function show()
+    {
         $products = Product::all();
-        return view('client.product.show')->with(['products'=>$products]);
-    }
-    public function detail(){
-
+        return view('client.product.show')->with(['products' => $products]);
     }
 
+    public function detail()
+    {
+
+    }
 
     public function find(Request $request)
     {
         if ($request->get('query')) {
             $query = $request->get('query');
-            $data = DB::table('products')
-                ->where('name', 'like', "%{$query}%")
-                ->get();
+            $data = Product::where('name', 'like', "%{$query}%")->get();
             $output = '<ul class="dropdown-menu" style="display:block; position:relative">';
             foreach ($data as $row) {
-                $page_url = "{{url('product/show/{$row->id}')}}";
-                $output .= '<li><a href="'.$page_url.'">' . $row->name . '</a></li>';
+                $page_url = url("product/show/{$row->id}");
+                $output .= '<li><a href="' . $page_url . '">' . $row->name . '</a></li>';
             }
             $output .= '</ul>';
             echo $output;
